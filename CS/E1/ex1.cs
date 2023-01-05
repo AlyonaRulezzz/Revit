047750d5afb310e99252e9ec3e4c8e0ba13ad268
Alyonka <Alyonazzz12@mail.ru>
2023-01-05 23:16:56 +0300

    first commit

---
+++ CS/E1/ex1.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ex1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string mes = "Ola)";
+            TaskDialog.Show("Задание 1", mes);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CS/HW/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace HW
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+           ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
