4ae95e3e0ba039e73ca35e7ad5e7e62c9f8fa1b0
Alyonka <Alyonazzz12@mail.ru>
2023-01-07 01:29:57 +0300

    ex1_start

--- CS/E1/ex1.cs
+++ CS/E1/ex1.cs
@@ -20,30 +20,87 @@ namespace E1
             Document doc = uiApp.ActiveUIDocument.Document;
 
             string mes = "";
-//            foreach (int item in walls_list)
-//              mes += item + " ";
 
-            FilteredElementCollector collector = new FilteredElementCollector(doc);
-            collector.OfCategory(BuiltInCategory.OST_Walls);
+            FilteredElementCollector collector_walls = new FilteredElementCollector(doc);
+            collector_walls.OfCategory(BuiltInCategory.OST_Walls);
 
             Wall wall = null;
-            foreach (Element elem in collector)
+            foreach (Element elem in collector_walls)
             {
                 wall = elem as Wall;
                 if (wall != null)
                 {
-                    mes += elem.Name + " = Name, " + elem.Id + " = Id, ";
+                    mes += elem.Name + ", " + elem.Id + "\n";
                 }
             }
 
+            SortedDictionary<string, string> rooms_with_walls =
+            new SortedDictionary<string, string>();
 
-            //List<int> walls_list = new List<int>((collector);
-            /*
-            List<int> walls_list = new List<int>();
-            walls_list.Add(0);
-            walls_list.Add(1);
-            walls_list.Add(2);
-            */
+            FilteredElementCollector collector_rooms = new FilteredElementCollector(doc);
+            collector_rooms.OfCategory(BuiltInCategory.OST_Rooms);
+            Room room = null;
+            foreach (Element elem in collector_rooms)
+            {
+                room = elem as Room;
+                if (room != null)
+                {
+                    //mes += elem.Name + ", " + elem.Id + "\n";
+                    Getinfo_Room(room);
+                    rooms_with_walls.Add(elem.Name, "");
+                }
+            }
+
+            foreach (string key in rooms_with_walls.Keys)
+            {
+                mes += rooms_with_walls[key] + " ------- из мапы\n";
+            }
+
+            void Getinfo_Room(Room rms)
+            {
+                string m = "Room: ";
+
+                //get the name of the room
+                m += "\nRoom Name: " + rms.Name;
+
+                //get the room position
+                LocationPoint location = rms.Location as LocationPoint;
+                XYZ point = location.Point;
+                m += "\nRoom position: " + XYZToString(point);
+
+                //get the room number
+                m += "\nRoom number: " + rms.Number;
+
+                IList<IList<Autodesk.Revit.DB.BoundarySegment>> segments = rms.GetBoundarySegments(new SpatialElementBoundaryOptions());
+                if (null != segments)  //the room may not be bound
+                {
+                    foreach (IList<Autodesk.Revit.DB.BoundarySegment> segmentList in segments)
+                    {
+                        m += "\nBoundarySegment of the room: ";
+                        foreach (Autodesk.Revit.DB.BoundarySegment boundarySegment in segmentList)
+                        {
+                            // Get curve start point
+                            XYZ start = boundarySegment.GetCurve().GetEndPoint(0);
+                            m += "\nCurve start point: " + XYZToString(start);
+                            // Get curve end point
+                            XYZ end = boundarySegment.GetCurve().GetEndPoint(1);
+                            m += " Curve end point: " + XYZToString(end);
+
+                            // Show the boundary elements
+                            m += "\nBoundary element id: " + boundarySegment.ElementId.IntegerValue;
+                        }
+                    }
+                }
+
+                //TaskDialog.Show("Revit", m);
+                mes += m + "\n\n\n";
+            }
+
+            // output the point's three coordinates
+            string XYZToString(XYZ point)
+            {
+                return "(" + point.X + ", " + point.Y + ", " + point.Z + ")";
+            }
 
             TaskDialog.Show("Задание 1", mes);
 
