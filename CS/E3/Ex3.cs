c4e0d7bfb5b188179fb51f1935d894e72f10122a
Alyonka <Alyonazzz12@mail.ru>
2023-01-11 19:37:27 +0300

    ex3_init

--- CS/E3/Ex3.cs
+++ CS/E3/Ex3.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,44 @@ using System.Threading.Tasks;
 
 namespace E3
 {
-    public class Ex3
+    [Transaction(TransactionMode.Manual)]
+    public class Ex3 : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+
+            string mes = "";
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+
+            using (Transaction tran = new Transaction(doc))
+            {
+                tran.Start("tran1");
+                foreach (Element elem in collector)
+                {
+                    if (elem is Room room)
+                    {
+                        if (room.Name.Contains("Помещение"))
+                        {
+                            mes += "Есть ошибки " + room.Id + "\n";
+                        }
+                    }
+                }
+
+                if (mes == "")
+                {
+                    mes += "Нет ошибок";
+                }
+                tran.Commit();
+            }
+
+            TaskDialog.Show("Задание 3", mes);
+
+            return Result.Succeeded;
+        }
     }
 }
