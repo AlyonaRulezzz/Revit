b03545ee45cfb6f1e9aee787c2091261796ecb7b
Alyonka <Alyonazzz12@mail.ru>
2023-01-12 15:04:27 +0300

    ex3

--- CS/E1/ex1.cs
+++ CS/E1/ex1.cs
@@ -2,11 +2,7 @@
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace E1
 {
